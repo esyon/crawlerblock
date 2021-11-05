78f11639eb7e1544884946c224a7ed8e0ed86ab6
ahirsch <Esyon4711#>
2021-11-05 10:50:37 +0100

    initial commit

---
+++ Core/ShopControl.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * This Software is the property of ESYON and is protected by copyright law - it is NOT Freeware.
+ * Any unauthorized use of this software without a valid license is a violation of the license agreement and will be
+ * prosecuted by civil and criminal law.
+ *
+ * @copyright      (C) ESYON GmbH
+ * @since              Version 1.0
+ * @author             Alexander Hirschfeld <support@esyon.de>
+ * @link               http://www.esyon.de
+ */
+
+declare(strict_types=1);
+
+namespace Esyon\CrawlerBlock\Core;
+
+use Jaybizzle\CrawlerDetect\CrawlerDetect;
+use OxidEsales\Eshop\Core\Registry;
+
+/**
+ * Class ViewConfig
+ * @package Esyon\MultiBaskets\Core
+ */
+class ShopControl extends ShopControl_parent
+{
+    /**
+     * start
+     *
+     * @param  mixed $sClass
+     * @param  mixed $sFunction
+     * @param  mixed $aParams
+     * @param  mixed $aViewsChain
+     * @return void
+     */
+    public function start($sClass = null, $sFunction = null, $aParams = null, $aViewsChain = null)
+    {
+        if (!$this->isAdmin()) {
+            $CrawlerDetect = new CrawlerDetect();
+            if ($CrawlerDetect->isCrawler()) {
+                Registry::getUtils()->showMessageAndExit('');
+            }
+        }
+
+        parent::start($sClass, $sFunction, $aParams, $aParams);
+    }
+}
---
+++ metadata.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * This Software is the property of ESYON and is protected by copyright law - it is NOT Freeware.
+ * Any unauthorized use of this software without a valid license is a violation of the license agreement and will be
+ * prosecuted by civil and criminal law.
+ *
+ * @copyright      (C) ESYON GmbH
+ * @since          Version 1.0
+ * @author         Alexander Hirschfeld <support@esyon.de>
+ * @link           http://www.esyon.de
+ */
+
+$sMetadataVersion = '2.0';
+
+$aModule = [
+    'id' => 'esy/crawlerblock',
+    'title' => [
+        'de' => 'ESYON Crawler-Blocker',
+        'en' => 'ESYON crawler block',
+    ],
+    'description' => [
+        'de' => 'Modul blockiert sämtliche bots / crawlers / spiders.',
+        'en' => 'Modul blocks all kind of bots / crawlers / spiders.',
+    ],
+    'thumbnail' => 'module.png',
+    'version' => '0.0.1',
+    'author' => 'ESYON GmbH',
+    'url' => 'https://www.esyon.de',
+    'email' => 'support@esyon.de',
+    'extend' => [
+        \OxidEsales\Eshop\Core\ShopControl::class => Esyon\CrawlerBlock\Core\ShopControl::class
+    ]
+];
