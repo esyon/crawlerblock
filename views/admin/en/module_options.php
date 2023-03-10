028f7cbbacbf94707564e91ea2b1411fe37deb52
esyon <35834723+esyon@users.noreply.github.com>
2023-03-10 12:10:14 +0100

    Create module_options.php

---
+++ views/admin/en/module_options.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * This Software is the property of ESYON and is protected by copyright law - it is NOT Freeware.
+ * Any unauthorized use of this software without a valid license is a violation of the license agreement and will be
+ * prosecuted by civil and criminal law.
+ *
+ * @copyright ESYON GmbH
+ * @since     Version 1.0
+ * @author    Alexander Hirschfeld <support@esyon.de>
+ * @link      https://www.esyon.de
+ */
+
+$aLang = [
+    'charset' => 'UTF-8',
+    'SHOP_MODULE_GROUP_ESYCrawlerBlockSettings' => 'Settings',
+    'SHOP_MODULE_esyUserAgentWhiteList' => 'Allowed User-Agents',
+    'HELP_SHOP_MODULE_esyUserAgentWhiteList' => 'A part of the User-Agent can also be specified here.',
+];
